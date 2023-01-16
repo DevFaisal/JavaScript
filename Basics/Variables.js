ad860b3bbc8343860a90cc49deab938b06d5280b
Faisal <74142521+DevFaisal@users.noreply.github.com>
2023-01-16 11:44:39 +0000

    Var added

---
+++ Basics/Variables.js
@@ -0,0 +1,13 @@
+const accountId = 1234;
+let accountName = "Faisal"
+var bankName = "J&K Bank"
+
+
+// accountId = 1932 // const can not be changed
+
+
+// prefer not to use var 
+// because of issue in block scope and functional scope
+
+
+console.table([accountId,accountName,bankName])
